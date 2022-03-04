1925b34f4c93bf865a05b2e3557eb5bf0b92fc59
ZachBouhin <zachb974@gmail.com>
2022-03-04 12:27:08 +0100

    new branch

--- projet/Agent.py
+++ projet/Agent.py
@@ -16,7 +16,7 @@ class Agent() :
         self.epsilon = 0
         self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        self.model = Linear_QNet(16,16,3)
+        self.model = Linear_QNet(10,10,3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
         
     
@@ -29,15 +29,15 @@ class Agent() :
         self.epsilon = 80 - self.n_games
         nb_col = game.get_nb_col()
         final_move = [0 for i in range(nb_col)] 
-        if random.randint(0, 200) > self.epsilon:
+        if random.randint(0, 500) <= self.epsilon:
             move = random.randint(0, nb_col -1)
             final_move[move] = 1
         else : 
             state0 = torch.tensor(state, dtype=torch.float)
             prediction = self.model(state0)
             move = torch.argmax(prediction).item()
-            print("-------",state0,"-------",prediction)
-            print("++*fdfdd", move,"fdfdfd", final_move)
+            #print("-------",state0,"-------",prediction)
+            #print("++*fdfdd", move,"fdfdfd", final_move)
             final_move[move%game.get_nb_col()] = 1
         return final_move
         
@@ -66,26 +66,27 @@ def train():
     plot_mean_scores = []
     total_score = 0
     agent = Agent()
-    game = Game(16,3)
+    game = Game(10,3)
     i = 0
     record = game.get_max_move()
     counter = 0
+    nwin = 0 
     while True:
         # get old state
-        print(game.get_grill())
-        print("     --------       ")
+        #print(game.get_grill())
+        #print("     --------       ")
 
 
 
         state_old = agent.get_state(game)
-        print(state_old)
-        print("     --------       ")
+        #print(state_old)
+        #print("     --------       ")
         # get move
         final_move = agent.get_action(state_old,game)
-        print(final_move)
-        print("     --------       ")
+        #print(final_move)
+        #print("     --------       ")
 
-        print("     --------       ")
+        #print("     --------       ")
 
         # perform move and get new state
 
@@ -99,11 +100,12 @@ def train():
 
         # remember
         agent.remember(state_old, final_move, reward, state_new, endgame)
-        print(counter)
-        print("+++++++++++++++")
+        #print(counter)
+        #print("+++++++++++++++")
         if endgame == True:
             # train long memory, plot result
-        
+            if reward > 2 : 
+                nwin += 1
             agent.n_games += 1
             agent.train_long_memory()
 
@@ -117,11 +119,12 @@ def train():
             total_score += counter
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+            plot(plot_mean_scores, plot_scores)
             game.reset()
             counter=0
 
-        print(i)
+
+        #print(i)
         i+=1
 
 if __name__ == '__main__':
